310d468b930a6bd2fdc6b0139c093ddd8073cea2
AnastasiosMa <tasos.mavrol@gmail.com>
2023-02-02 15:21:35 +0200

    finalised percentage calculation script

--- part1_adaptive/other/percentage_difficulties_calculation.m
+++ part1_adaptive/other/percentage_difficulties_calculation.m
@@ -21,6 +21,8 @@ for i = 1:110
             excerpt_data{:,emo_idxs(sorted_labels(1+j))})/height(excerpt_data);
         false_label_difficulty = sum(excerpt_data{:,emo_idxs(sorted_labels(1))}<...
             excerpt_data{:,emo_idxs(sorted_labels(1+j))})/height(excerpt_data);
+        [h,p,ci,stats] = ttest(excerpt_data{:,emo_idxs(sorted_labels(1))},...
+            excerpt_data{:,emo_idxs(sorted_labels(1+j))});
         dist_ratings([i-1]*3+j).Label = strjoin([emo_labels(target_label_idx),emo_labels(sorted_labels(1+j))]);
         dist_ratings([i-1]*3+j).Name = i;
         dist_ratings([i-1]*3+j).TargetEmo = sorted(1);
@@ -28,13 +30,18 @@ for i = 1:110
         dist_ratings([i-1]*3+j).Difficulty = difficulty;
         dist_ratings([i-1]*3+j).DifficultyInverted = false_label_difficulty;
         dist_ratings([i-1]*3+j).Distance = dists(j);
+        dist_ratings([i-1]*3+j).Ttest = stats.tstat;
         dist_ratings([i-1]*3+j).Label1 = target_label_idx
         dist_ratings([i-1]*3+j).Label2 = sorted_labels(j+1)
         dist_ratings([i-1]*3+j).Soundtrack = mean_scores{i,'Soundtrack'};
     end
 end
 dist_ratings=struct2table(dist_ratings);
-dist_ratings = sortrows(dist_ratings,'Difficulty','descend');
+dist_ratings = sortrows(dist_ratings,'Ttest','descend');
+%remove trials with low target emo means
+p = prctile(dist_ratings.TargetEmo,12);
+idx = find(dist_ratings.TargetEmo>p);
+dist_ratings = dist_ratings(idx,:);
 
 for k = 1:4
     cell_trials{k} = dist_ratings(find(dist_ratings.Label1 == k),:);
@@ -47,37 +54,58 @@ trials=[];
 for i = 1:4
     trials = [trials; cell_trials{i}];
 end
-trials = sortrows(trials,5,'ascend');
+trials = sortrows(trials,'Ttest','descend');
+
+trials.Distance = trials.Ttest;
+trials = removevars(trials,["Ttest","Difficulty","DifficultyInverted"]);
+trials{:,'Trials'} = [1:height(trials)]';
+%writetable(trials,'trials.csv')
 
 %figure,plot(sort(dist_ratings{:,'Distance'})), xlabel('Trials'),ylabel('Distance')
 figure
-subplot(1,2,1)
+subplot(1,3,1)
 for i = 1:4
     hold on
     plot(cell_trials{i}.Difficulty)
 end
 legend(emo_labels),xlabel('Trials'),ylabel('Difficulty'),title('Percentages of true answer')
 hold off
-subplot(1,2,2)
+subplot(1,3,2)
 for i = 1:4
     hold on
     plot(sort(cell_trials{i}.Distance,'descend'))
 end
-legend(emo_labels),xlabel('Trials'),ylabel('Distance'),title('Distances between true and false answer')
+legend(emo_labels),xlabel('Trials'),ylabel('Distance'),title('Distances of true and false answer')
 hold off
-%writetable(trials,'trials.csv')
-
-%compare mean ratings and raw ratings
+subplot(1,3,3)
 for i = 1:4
-[rho(i),pval(i)] =corr(ratings(:,i),mean_excerpt_values(:,i));
+    hold on
+    plot(sort(cell_trials{i}.Ttest,'descend'))
 end
+legend(emo_labels),xlabel('Trials'),ylabel('Ttest value'),title('Ttest of true and false answer')
+hold off
 
-%compare Difficulty (percentage) and Distance
-[rho,pval] = corr(trials.Difficulty,trials.Distance);
+%compare Difficulty metrics
 figure
-scatter(trials.Difficulty,trials.Distance)
-xlabel('Difficulty (percentage of true answer being higher)')
+subplot(1,3,1)
+[rho,pval] = corr(trials.Difficulty,trials.Distance);
+scatter(trials.Difficulty,rescale(trials.Distance,0,1))
+xlabel('Percentage (percentage of true answer being higher)')
 ylabel('Distance (distance of mean emotion ratings)')
-title(['Scatterplot between difficulty metrics: r = ', num2str(round(rho,2))])
+title(['Scatterplot of Percentage and Distance: r = ', num2str(round(rho,2))])
+subplot(1,3,2)
+[rho,pval] = corr(trials.Ttest,trials.Distance);
+scatter(trials.Difficulty,trials.Distance)
+xlabel('Tstatistic ')
+ylabel('Distance (distance of mean emotion ratings)')
+title(['Scatterplot of Ttest and Distance: r = ', num2str(round(rho,2))])
+subplot(1,3,3)
+[rho,pval] = corr(trials.Difficulty,trials.Ttest);
+scatter(trials.Difficulty,trials.Ttest)
+xlabel('Percentage (percentage of true answer being higher)')
+ylabel('Tstatistic')
+title(['Scatterplot between Percentage and Ttest: r = ', num2str(round(rho,2))])
+
+
 
 
