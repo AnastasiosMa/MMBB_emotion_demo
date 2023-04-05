f328d1894d024110a45d850fdc4fbc0585e46de7
AnastasiosMa <tasos.mavrol@gmail.com>
2023-04-05 12:07:31 +0300

    removed jatos folder

--- jatos/jspsych/dist/custom/randomizationTrials.js
+++
@@ -1,9 +0,0 @@
-var querySequence = window.location.search;
-const urlSequence = new URLSearchParams(querySequence);
-
-var nextTask = urlParams.get('next')
-
-console.log(nextTask)
-
-//var whereSend = "https://pasoneto.github.io/MMBB/src/batteries/" +  + ".html";
-//whichNext()
