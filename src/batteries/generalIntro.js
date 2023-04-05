2231e2e9e7c00d03623a867ede1b17a136bcd705
AnastasiosMa <tasos.mavrol@gmail.com>
2023-04-05 20:38:14 +0300

    added difficulty question

--- src/batteries/generalIntro.js
+++ src/batteries/generalIntro.js
@@ -1,7 +1,7 @@
 var preloadTest = {
     type: jsPsychPreload,
     audio: ['./songs/movementTapAudio/elPesebre.mp3'],
-    images: ['./images/muteButton.png'],
+    images: ['./images/muteButton.png','./images/loading2.gif'],
     auto_preload: false,
 }
 
