b309c621bbf646c20cf0f80ac50957c231c27793
AnastasiosMa <tasos.mavrol@gmail.com>
2023-05-26 14:34:21 +0300

    irt visualisation

--- part1_adaptive/other/analysis/pilot_irt_conversion.m
+++ part1_adaptive/other/analysis/pilot_irt_conversion.m
@@ -23,11 +23,33 @@ legend(emo_labels);
 difficulty_scores = data{:,difficulty_col+1:difficulty_col+60};
 for k=1:size(difficulty_scores,1)
     for i=1:size(difficulty_scores,2)
-        idx(k,i) = find(difficulty_scores(k,i)==trials.Distance,1);
+        trial_num(k,i) = find(difficulty_scores(k,i)==trials.Distance,1);
     end
 end
 for i=1:height(trials)
-    occurences(i) = sum(sum(idx==i));
+    occurences(i) = sum(sum(trial_num==i));
 end
 figure
 scatter(1-rescale(trials.Distance),occurences,'filled')
+%% Correctness ratio / Difficulty scatterplot
+responses = data{:,2:61};
+trial_num_v= trial_num(:); responses_v = responses(:);
+correct_responses = trial_num_v(find(responses_v));
+[counts_trial,group_trial] = groupcounts(sort(trial_num_v));
+[counts_correct,group_correct] = groupcounts(sort(correct_responses));
+for i = 1:height(trials)
+    idx_trial = find(group_trial==i);
+    idx_correct = find(group_correct==i);
+    if idx_trial
+        if idx_correct
+            trial_response_ratio(i) = counts_correct(idx_correct)/counts_trial(idx_trial);
+        else
+            trial_response_ratio(i) = 0;
+        end
+    else
+        trial_response_ratio(i) = nan;
+    end
+end
+
+figure
+scatter(1-rescale(trials.Distance),trial_response_ratio,'filled');
